0b8d41a6c2afb4eed701aa0e98a9aac8e3fc30b1
DanMurphyy <highquality1996@gmail.com>
2025-04-01 03:24:22 +0500

    upgraded version

--- app/src/main/java/com/hfad/ismlarmanosi2023/remote/Constants.kt
+++ app/src/main/java/com/hfad/ismlarmanosi2023/remote/Constants.kt
@@ -1,7 +1,7 @@
 package com.hfad.ismlarmanosi2023.remote
 
 object Constants {
-    const val VERSION_NAME = "4.2"
+    const val VERSION_NAME = "4.3"
     const val REMOTE = "remote"
     const val APPPREFS = "app_prefs"
     const val REMOTECONFIGID = "iq6tV8T1TtWZ1ZW4r0ii"
